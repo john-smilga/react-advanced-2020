9cd4c57abc3a959332b01da768bcab7e684837f5
john-smilga <johnsmilga@gmail.com>
2020-10-27 21:19:28 -0700

    10-switched prop types to permanent domain

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/tutorial/10-prop-types/final/index.js
+++ src/tutorial/10-prop-types/final/index.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import Product from './Product';
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
+import React from 'react'
+import Product from './Product'
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 // import defaultImage from '../../../assets/default-image.jpeg';
-const url = 'https://course-api.netlify.app/api/react-prop-types-example';
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-prop-types-example'
 
 const Index = () => {
-  const { products } = useFetch(url);
+  const { products } = useFetch(url)
   return (
     <div>
       <h2>products</h2>
       {/* <img src={defaultImage} /> */}
       <section className='products'>
         {products.map((product) => {
-          return <Product key={product.id} {...product} />;
+          return <Product key={product.id} {...product} />
         })}
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
