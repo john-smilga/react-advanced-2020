c8d3ba5b49e72e9e66b8c46fb6b63b40b0eae195
john-smilga <johnsmilga@gmail.com>
2020-10-27 22:12:52 -0700

    9-custom hooks switched to permanent domain

--- src/tutorial/9-custom-hooks/final/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/final/1-fetch-example.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useFetch } from './2-useFetch';
-const url = 'https://course-api.netlify.app/api/javascript-store-products';
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './2-useFetch'
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/javascript-store-products'
+
 const Example = () => {
-  const { loading, products } = useFetch(url);
-  console.log(products);
+  const { loading, products } = useFetch(url)
+  console.log(products)
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
     </div>
-  );
-};
+  )
+}
 
-export default Example;
+export default Example
--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { useFetch } from './2-useFetch';
-const url = 'https://course-api.netlify.app/api/javascript-store-products';
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './2-useFetch'
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/javascript-store-products'
+
 const Example = () => {
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
 
   const getProducts = async () => {
-    const response = await fetch(url);
-    const products = await response.json();
-    setProducts(products);
-    setLoading(false);
-  };
+    const response = await fetch(url)
+    const products = await response.json()
+    setProducts(products)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    getProducts();
-  }, [url]);
-  console.log(products);
+    getProducts()
+  }, [url])
+  console.log(products)
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
     </div>
-  );
-};
+  )
+}
 
-export default Example;
+export default Example
