875cb4c68c3e04d28f809074331c4bdd68a1d8c5
asbaden <gusbaden96@gmail.com>
2021-03-18 20:20:42 -0700

    carrying on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Setup from "./tutorial/1-useState/setup/1-error-example"
+import Final from "./tutorial/1-useState/final/2-useState-basics"
 
 
 function App() {
   return (
     <div className='container'>
       <Setup />
+      <Final />
     </div>
   )
 }
