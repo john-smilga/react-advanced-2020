71bbb757e0a76577c8829a867fa03b35c70e689a
Hashan Eranga <ccmrhe@gmail.com>
2021-05-28 01:24:33 +0530

    use state handling in use State array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
+
 
 function App() {
   return (
     <div className="container">
       <Setup/>
+
     </div>
   )
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+  return(
+    <>
+      {
+        people.map((person) => {
+          const {id, name} = person
+          return <div className="item" key={id}>
+            <h4>{name}</h4>
+          </div>
+        })
+      }
+      <button className="btn" type='button' onClick={() => setPeople([])}>
+        clear item
+      </button>
+    </>
+  )
 };
 
 export default UseStateArray;
