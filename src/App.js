baa3ceadd7e415dfcfda90ce621fe975667d2294
Hashan Eranga <ccmrhe@gmail.com>
2021-05-27 23:08:14 +0530

    use the use state in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Setup from './tutorial/1-useState/setup/1-error-example'
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
+      <Setup/>
     </div>
   )
 }
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = ' Hashan Eranga ';
+
+  const handleClick = () =>{
+    console.log(title);
+  } 
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+          change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
