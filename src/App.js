dd35ed98c09bda976f7729214d0b67661826356c
Shawon Saha <shawondroid@gmail.com>
2021-06-04 01:23:54 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <ErrorExample />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
