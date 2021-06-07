3e3864332d804b57da0e63a245406ce4560fbe50
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-06-07 20:12:19 -0300

    delete lines to match video's setup file
    /setup/2-multiple-input.js isn't matching the setup file used in the tutorial's video. Deleting some lines gets the file to match whats is displayed on 6:50:32

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -9,21 +9,10 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
-  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
-    } else {
-      console.log('empty values');
-    }
+    console.log(firstName, email)
   };
   return (
     <>
@@ -51,15 +40,7 @@ const ControlledInputs = () => {
           </div>
           <button type='submit'>add person</button>
         </form>
-        {people.map((person, index) => {
-          const { id, firstName, email } = person;
-          return (
-            <div className='item' key={id}>
-              <h4>{firstName}</h4>
-              <p>{email}</p>
-            </div>
-          );
-        })}
+        
       </article>
     </>
   );
