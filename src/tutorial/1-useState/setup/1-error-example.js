986aea9b3a486afe7cb302e2675d2dd07114c015
Shawon Saha <shawondroid@gmail.com>
2021-06-07 21:41:15 +0600

    UseState - Simple Use Case

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const title = "hello world";
+  const handleClick = () => {
+    console.log("Hi there");
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        click to change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
