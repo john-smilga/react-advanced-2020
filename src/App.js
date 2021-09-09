08b04c0154577992d4fb14e244f42177113a2235
Andrew Cruz <ajdat10@gmail.com>
2021-09-09 04:28:23 -0400

    changing titles in error example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Setup from './tutorial/1-useState/setup/1-error-example'
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React Tutorial</h2>
+      <Setup/>
     </div>
   )
 }
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  let title = "Random Title"
+  
+  const handleClick = () => {
+    title = "Hello People"
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>Change Title</button>
+    </React.Fragment>);
 };
 
 export default ErrorExample;
