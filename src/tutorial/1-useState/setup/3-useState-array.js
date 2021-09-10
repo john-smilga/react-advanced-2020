1ade5934afa168ce577be086eca6362c8451ea4f
Andrew Cruz <ajdat10@gmail.com>
2021-09-09 20:07:53 -0400

    using setState with arrays. Creating function to delete all and individually

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,32 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  //Option 1 for creating a funtion for setState
+  // const eventHandler = () => {
+  //   setPeople([])
+  // }
+  
+  const removeName = (id) => {
+    let newPeople = people.filter((person)=> person.id !== id)
+    setPeople(newPeople)
+  }
+
+  return <>
+    {
+      people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeName(id)}>remove name</button>
+          </div>
+        )
+      })
+    }
+    <button className='btn' onClick={() => setPeople([])}>Clear people</button>
+  </>
 };
 
 export default UseStateArray;
