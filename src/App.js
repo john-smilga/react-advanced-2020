c8aa2907fb8ac4851721b133047df694aeaba206
Kiril Komitski <k.komitski94@gmail.com>
2022-01-26 17:27:34 +0000

    Spread operator, object state change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import State from './tutorial/1-useState/setup/3-useState-array';
+import State from './tutorial/1-useState/setup/4-useState-object';
 // import Final from './tutorial/1-useState/final/2-useState-basics';
 
 function App() {
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const [person, setPerson] = useState({ name: 'Timmy', age: 24, message: 'Halleo Meototos' });
+
+	const changeMessage = () => {
+		// set the new state value as all person values but change "message" value
+		setPerson({ ...person, message: 'hello world' });
+	};
+
+	console.log(person);
+
+	return (
+		<>
+			<h3>{person.name}</h3>
+			<h3>{person.age}</h3>
+			<h3>{person.message}</h3>
+			<button className='btn' onClick={changeMessage}>
+				Change Message
+			</button>
+		</>
+	);
 };
 
 export default UseStateObject;
