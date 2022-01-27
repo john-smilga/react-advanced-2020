a3197da8d4b3d438edad12d49ed4ae651d678a85
Kiril Komitski <k.komitski94@gmail.com>
2022-01-27 14:09:58 +0000

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import State from './tutorial/1-useState/setup/5-useState-counter';
-import StateArr from './tutorial/1-useState/setup/3-useState-array';
-// import Final from './tutorial/1-useState/final/2-useState-basics';
+import State from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
 	return (
 		<div className='container'>
 			<State />
-			<StateArr />
-			{/* <Final /> */}
 			<button
 				style={{ padding: '0.25rem 2rem', marginTop: '4rem' }}
 				onClick={(_) => console.clear()}
 			>
-				CLEAR
+				CLEAR LOG
 			</button>
 		</div>
 	);
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+
+	// useEffect(() => {doSomething, [everyTimeThisChanges]})
+	useEffect(() => {
+		// calls after every render
+		if (value >= 1) {
+			document.title = `New Messages (${value})`;
+		}
+	}, [value]); // runs on change of the values passed here
+
+	console.log('render component');
+	return (
+		<>
+			<h1>{value}</h1>
+			<button className='btn' onClick={() => setValue(value + 1)}>
+				click
+			</button>
+		</>
+	);
 };
 
 export default UseEffectBasics;
