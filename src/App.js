f8c67322898a2952baedccf68d54165341f75db8
sumit <srchavhan29@gmail.com>
2022-05-08 20:04:48 +0530

    Solve Q.1.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Setup from './tutorial/1-useState/setup/1-error-example'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   )
 }
 
 export default App
+
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let clickHandler = () => {
+    title = "second title"
+    console.log(title)
+  }
+  let title = "First title"
+  return <React.Fragment>
+    <h1>Error example</h1>
+    <h2>{title}</h2>
+    <button className='btn' onClick={clickHandler}>Change Title</button>
+  </React.Fragment>
 };
 
 export default ErrorExample;
