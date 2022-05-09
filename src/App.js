a4fd4d66c0d8b26503c853b5f0f0301d1da5635e
sumit <srchavhan29@gmail.com>
2022-05-09 21:04:18 +0530

    solve Q1.4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/3-useState-array'
+import Setup from './tutorial/1-useState/setup/4-useState-object'
 function App() {
   return (
     <div className='container'>
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Harry",
+    age: 25,
+    message: "random message"
+  })
+
+  const changeMessage = ()=>{
+    setPerson({...person, message:"Hello World"})
+  }
+
+  return <div>
+    <h2>useState object example</h2>
+    <h1>{person.name}</h1>
+    <h1>{person.age}</h1>
+    <h1>{person.message}</h1>
+    <button className='btn' onClick={changeMessage}>Change Message</button>
+  </div>
 };
 
 export default UseStateObject;
