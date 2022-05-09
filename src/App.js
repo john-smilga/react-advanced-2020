a2a7bd8910d12d31c1b132a488bdcfe808de1f30
sumit <srchavhan29@gmail.com>
2022-05-09 20:39:25 +0530

    solve Q1.3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
+import { useState } from "react";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const clearAll = () => {
+    setPeople([]);
+  };
+
+  const clickHandler = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const listing = (person) => {
+    const { id, name } = person;
+    return (
+      <>
+        <h4 key={id} className="item">
+          {name}
+          <span>
+            <button className="btn" onClick={() => clickHandler(id)}>
+              Clear
+            </button>
+          </span>
+        </h4>
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <h1>useState Array</h1>
+      {people.map(listing)}
+      <button className="btn" onClick={clearAll}>
+        clear all
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
