dc1e8339691707454b5d154fc8cd06b9113c146f
Lance Liu <lanceliumeng89@gmail.com>
2022-06-25 15:09:59 +1000

    use callback function to handle if we need to pass any parameter into the onclick function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Final from "./tutorial/1-useState/final/2-useState-basics";
-import SetupState from "./tutorial/1-useState/setup/2-useState-basics";
+import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 function App() {
   return (
     <div className="container">
-      <SetupState />
+      <UseStateArray />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const clearClickHandler = () => {
+    setPeople([]);
+  };
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={clearClickHandler}>
+        Clear
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
