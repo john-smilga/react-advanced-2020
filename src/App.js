7a6987f1a8c4f76ffd63069cc8d1bb158910c606
Lance Liu <lanceliumeng89@gmail.com>
2022-06-25 16:23:17 +1000

    useState operate object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Final from "./tutorial/1-useState/final/2-useState-basics";
-import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
+import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
 function App() {
   return (
     <div className="container">
-      <UseStateArray />
+      <UseStateObject />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const initObj = {
+    name: "Lance",
+    age: "33",
+    message: "random message",
+  };
+  const [person, setPerson] = useState(initObj);
+  const { name, age, message } = person;
+  const msgChangeHanlder = () => {
+    setPerson({ ...person, message: "new message" });
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={msgChangeHanlder}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
