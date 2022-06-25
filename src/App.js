4680b36e52606c201485bf6c489595ad92ce37bd
Lance Liu <lanceliumeng89@gmail.com>
2022-06-25 17:53:31 +1000

    simple counter by using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Final from "./tutorial/1-useState/final/2-useState-basics";
-import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
+import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
 function App() {
   return (
     <div className="container">
-      <UseStateObject />
+      <UseStateCounter />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+  const increaceHandler = () => {
+    setCount(count + 1);
+  };
+  const clearHandler = () => {
+    setCount(0);
+  };
+  const decreaceHandler = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <seciont style={{ margin: "4rem 0" }}>
+        <h2>simple counter</h2>
+        <h1>{count}</h1>
+      </seciont>
+      <button type="button" className="btn" onClick={increaceHandler}>
+        +
+      </button>
+      <button type="button" className="btn" onClick={clearHandler}>
+        Clear
+      </button>
+      <button type="button" className="btn" onClick={decreaceHandler}>
+        -
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
