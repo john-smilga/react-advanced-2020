d1ac3f628cbc3c303e4512721e78e01a4ec87762
JK-Kim4 <jongbell4@gmail.com>
2022-08-02 16:11:18 +0900

    using useState show modal

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,36 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(name){
+      setShowModal(true);
+      setPeople([...people, {id:new Date().getTime().toString(), name}]);
+      setName('');
+    }
+    else{
+      setShowModal(true);
+    }
+  }
+
+  return <>
+  {showModal && <Modal/>}
+  <form onSubmit={handleSubmit} className="form">
+    <div>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+    </div>
+    <button type='submit'>add</button>
+  </form>
+  {people.map((person) => {
+    return <div key={person.id}>
+      <h4>{person.name}</h4>
+    </div>
+  })}
+  </>;
 };
 
 export default Index;
