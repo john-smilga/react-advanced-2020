01bce73d82a1164fe06b20cb7617c01e2120c075
JK-Kim4 <jongbell4@gmail.com>
2022-08-02 16:45:37 +0900

    update

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 
-const Modal = ({modalContent}) => {
+const Modal = ({modalContent, closeModal}) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+
   return <div className='modal'><p>{modalContent}</p></div>;
 };
 
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -15,6 +15,10 @@ const reducer = (state, action) => {
   };
   if(action.type === 'CLOSE_MODAL'){
     return {...state, isModalOpen:false}
+  };
+  if(action.type === 'REMOVE_ITEM'){
+    const newPeople = state.people.filter((person) => person.id === action.payload);
+    return {...state, people:newPeople}
   }
   throw new Error('No Matching action type');
 }
@@ -43,7 +47,7 @@ const Index = () => {
     dispatch({type:'CLOSE_MODAL'});
   }
   return <>
-  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  {state.isModalOpen && <Modal modalContent={state.modalContent} closeModal={closeModal}/>}
   <form onSubmit={handleSubmit} className="form">
     <div>
       <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
@@ -51,8 +55,9 @@ const Index = () => {
     <button type='submit'>add</button>
   </form>
   {state.people.map((person) => {
-    return <div key={person.id}>
+    return <div key={person.id} className="item">
       <h4>{person.name}</h4>
+      <button onClick={() => dispatch({type:"REMOVE_ITEM", payload:person.id})}>remove</button>
     </div>
   })}
   </>;
