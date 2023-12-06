bdc196ac120e42e2216c5f9624f7d19331ef06f8
SaifBataineh <76657049+saefbataineh@users.noreply.github.com>
2023-12-06 15:58:19 +0300

    new branch created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Setup from "./tutorial/1-useState/setup/4-useState-object"
-// import Final from "./tutorial/1-useState/final/1-error-example"
 function App() {
   return (
     <div className="container">
