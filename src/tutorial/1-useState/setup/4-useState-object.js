8a8941cc2a416955d07f99039c6c9b2d93d4aacc
SaifBataineh <76657049+saefbataineh@users.noreply.github.com>
2023-12-07 09:29:01 +0300

    UseState - Multiple State Values
    UseState - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
 const UseStateObject = () => {
-  const [note, setNote] = useState(false)
-  const [person, setPerson] = useState({
-    name: "Saif",
-    age: 36,
-    message: "Hello World",
-  })
+  // const [note, setNote] = useState(false)
+  // const [person, setPerson] = useState({
+  //   name: "Saif",
+  //   age: 36,
+  //   message: "Hello World",
+  // })
+
+  const [name, setName] = useState("Saif")
+  const [age, setAge] = useState(36)
+  const [message, setMessage] = useState("Hello World")
+
+  // const changeMessage = () => {
+  //   setMessage((prev) => {
+  //     return {
+  //       ...prev,
+  //       message: prev.message === "Hello World" ? "hi" : "Hello World", //// this when using object as above
+  //     }
+  //   })
+  // }
   const changeMessage = () => {
-    setPerson((prev) => {
-      return {
-        ...prev,
-        message: prev.message === "Hello World" ? "hi" : "Hello World",
-      }
-    })
+    message === "Hello World" ? setMessage("hi") : setMessage("Hello World")
   }
 
   return (
     <>
-      <h2>{person.name}</h2>
-      <h3>{person.age}</h3>
-      <h4>{person.message}</h4>
-      <button onClick={changeMessage.bind()} className="btn">
+      <h2>{name}</h2>
+      <h3>{age}</h3>
+      <h4>{message}</h4>
+      <button onClick={() => changeMessage()} className="btn">
         Change Message
       </button>
     </>
