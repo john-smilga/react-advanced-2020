3582233f355a067cc9b48067c1b02397716f0838
SaifBataineh <76657049+saefbataineh@users.noreply.github.com>
2023-12-10 13:39:49 +0300

    3-useEffect-fetch-data
    3-useEffect-fetch-data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup"
+import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data"
 // import Setup from "./Course-Projects/1-birthday-buddy"
 function App() {
   return (
